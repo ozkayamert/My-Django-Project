6d7119736b2277fd7d491d08a6edc192a6190843
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-01 22:24:54 +0300

    Categories named category and item were created. Super user was created. 2 products were uploaded.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from item.models import Category, Item
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html') # Anladığım kadarıyla bu kısım templates kısmını kullanıyor yani templates ile alakalı
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    }) # Anladığım kadarıyla bu kısım templates kısmını kullanıyor yani templates ile alakalı
 # bir bölüm olabilir burası.
 
 def contact(request):
