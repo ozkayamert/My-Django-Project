e94ee8ef011e3d82b104e1876c362a63bef98e5c
Mert Ozkaya <ozkayamert35@gmail.com>
2024-04-06 15:58:52 +0300

    User login and user registration pages were created.

--- puddle/urls.py
+++ puddle/urls.py
@@ -19,11 +19,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
